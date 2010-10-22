7450273e28e274844597211c708f56b6e1b91207
Alex Roberts <bse@Tank.local>
2010-10-22 11:42:45 -0700

    Rearranged things to work the Rails Way™

--- lib/acts_as_licensable.rb
+++ lib/acts_as_licensable.rb
@@ -1,4 +1,9 @@
+require "active_record"
 
+require "acts_as_licensable/acts_as_licensable"
 require "acts_as_licensable/license"
 require "acts_as_licensable/licensing"
 
+if defined?(ActiveRecord::Base)
+  ActiveRecord::Base.extend ActsAsLicensable::Licensable
+end
--- lib/acts_as_licensable/acts_as_licensable.rb
+++ lib/acts_as_licensable/acts_as_licensable.rb
@@ -1,18 +1,29 @@
 module ActsAsLicensable
   module Licensable
+    def self.included(base)
+      base.send :extend, ClassMethods
+    end
     
-    ##
-    # Makes a model licensable
-    #
-    # Example:
-    #    class Book < ActiveRecord::Base
-    #      acts_as_licensable
-    #    end
-    def acts_as_licensable
-      class_eval do
-        has_one :licensing, :dependent => :destroy, :include => :license
-        has_one :license, :through => :licensing
+    module ClassMethods
+      ##
+      # Makes a model licensable
+      #
+      # Example:
+      #    class Book < ActiveRecord::Base
+      #      acts_as_licensable
+      #    end
+      def acts_as_licensable(options = {})
+        class_eval do
+          has_one :licensing, :dependent => :destroy, :include => :license
+          has_one :license, :through => :licensing
+        end
+        
+        send :include, InstanceMethods
       end
+    end
+    
+    module InstanceMethods
+      
     end
   end
 end
