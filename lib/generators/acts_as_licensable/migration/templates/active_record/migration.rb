7678434071ee618ff9f2ba1435d00e5b17dbecb0
Alex Roberts <bse@Tank.local>
2010-10-21 23:20:59 -0700

    Built out table migrations

--- lib/generators/acts_as_licensable/migration/templates/active_record/migration.rb
+++ lib/generators/acts_as_licensable/migration/templates/active_record/migration.rb
@@ -1,15 +1,33 @@
 class ActsAsLicensableMigration < ActiveRecord::Migration
   def self.up
     create_table :licenses do |t|
+      # "GNU Public License", "Creative Commons Attribution"
+      t.string  :name
+      
+      # "GPL", "CC-BY"
+      t.string  :short_name
+      
+      t.string  :version
+      t.string  :url
+      
+      # CODE_LICENSE; CONTENT_LICENSE; CC_LICENSE; OTHER_LICENSE (incl. public domain)
+      t.integer :type
+
+      t.text    :description
+
+      t.timestamps
     end
     
-    create_table :licensed do |t|
+    create_table :licensings do |t|
+      t.references :license
+      t.references :licensable, :polymorphic => true
       
+      t.datetime :created_at
     end
   end
   
   def self.down
-    drop_table :licensed
+    drop_table :licensings
     drop_table :licenses
   end
 end
