bfec1c52804895eab347c4020a28cfe62a4f0da4
Alex Roberts <bse@Tank.local>
2010-10-22 21:12:29 -0700

    Added some missing version numbers

--- lib/acts_as_licensable.rb
+++ lib/acts_as_licensable.rb
@@ -6,6 +6,11 @@ require "acts_as_licensable/license"
 require "acts_as_licensable/licensing"
 require "acts_as_licensable/acts_as_licensable_helper"
 
+LICENSE_KIND_CODE = 1
+LICENSE_KIND_CONTENT = 2
+LICENSE_KIND_OTHER = 3
+
+
 if defined?(ActiveRecord::Base)
   ActiveRecord::Base.send :include, ActsAsLicensable::Licensable
 end
