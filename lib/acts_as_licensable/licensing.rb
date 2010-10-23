ea8ff358829ec465ab2ba680eec20218ab15eee9
Alex Roberts <bse@Tank.local>
2010-10-22 20:47:23 -0700

    Removed module wrapper

--- lib/acts_as_licensable/licensing.rb
+++ lib/acts_as_licensable/licensing.rb
@@ -1,10 +1,8 @@
-module ActsAsLicensable
-  class Licensing < ::ActiveRecord::Base #:nodoc:
-    belongs_to :license
-    belongs_to :licensable, :polymorphic => true
-    
-    validates_presence_of :license_id
-    
-    validates_uniqueness_of :license_id, :scope => :licensable_id
-  end
+class Licensing < ActiveRecord::Base #:nodoc:
+  belongs_to :license
+  belongs_to :licensable, :polymorphic => true
+
+  validates_presence_of :license_id
+
+  validates_uniqueness_of :license_id, :scope => :licensable_id
 end
